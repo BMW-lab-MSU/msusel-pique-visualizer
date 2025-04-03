e7d2777bec80855041c1529ff67317d4444e6f36
kaveen <kaveenliyanage@yahoo.com>
2025-04-03 12:08:51 -0600

    Imapact calculation to be fixed

--- src/composites/ConfigurationContainer/ImportanceAdjustment/AdjustmentTable/SingleRowInTable.tsx
+++ src/composites/ConfigurationContainer/ImportanceAdjustment/AdjustmentTable/SingleRowInTable.tsx
@@ -110,6 +110,7 @@ const SingleTableRow: React.FC<SingleTableRowProps> = ({
                 {recalculatedWeight.toFixed(2)}
             </Table.Cell>
             <Table.Cell align='center' justify={'center'}>
+                {/*TODO: Imapct calculation recheck*/}
             {Math.max(0, (characteristicSlider - recalculatedWeight)).toFixed(2)}
             </Table.Cell>
         </Table.Row>
