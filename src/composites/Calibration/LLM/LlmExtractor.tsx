f11a8aa4157fcba48f65831c4ec182e742cb6731
kaveen <kaveenliyanage@yahoo.com>
2025-04-10 11:27:29 -0600

    create a dummy chatbot

--- src/composites/Calibration/LLM/LlmExtractor.tsx
+++ src/composites/Calibration/LLM/LlmExtractor.tsx
@@ -52,4 +52,54 @@ export const ChatbotUI = () => {
     )
 }
 
+export const LlmExtractor = () => {
+    const [messages,setMessages]=useState([])
+    const [value,setValue]=useState("")
+
+
+    const sendMessage=async()=>{
+        const newMsg={role:"user",content:value}
+        setMessages([...messages,newMsg])
+        const payload={
+            "model": "llama3.2:latest",
+            "messages": [
+                { "role": "user", "content": value }
+            ],
+            "stream":false
+        }
+        axios.post('http://127.0.0.1:11434/api/chat',payload).then(res=>{
+            console.log(res.data.message)
+            setMessages(prev=>[...prev,res.data.message])
+            setValue("")
+        }).catch(err=>console.log(err))
+    }
+
+    return (
+        <>
+            <div className='chat-box'>
+                <div className="chat-innerbox">
+                    <div className="messages">
+                        {
+                            messages?.map((msg,index)=>(
+                                <div key={index} className={msg.role}>{msg.content}</div>
+                            ))
+                        }
+                    </div>
+                    {/*<div className='input-box'>*/}
+                    {/*    <input type="text"*/}
+                    {/*           className='input-control'*/}
+                    {/*           placeholder='Type Something'*/}
+                    {/*           value={value}*/}
+                    {/*           onChange={(e)=>setValue(e.target.value)}></input>*/}
+                    {/*    <div className='send' onClick={sendMessage}><IoMdSend /></div>*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+
+
 export default ChatbotUI;
